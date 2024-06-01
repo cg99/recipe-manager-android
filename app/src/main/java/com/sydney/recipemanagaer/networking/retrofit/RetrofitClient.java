19ff9dcb133b8255db3cb3c46a4ee1ce6ee65a7f
cg99 <darkinfinitespace@gmail.com>
2024-06-01 18:59:00 +1000

    change base url

--- app/src/main/java/com/sydney/recipemanagaer/networking/retrofit/RetrofitClient.java
+++ app/src/main/java/com/sydney/recipemanagaer/networking/retrofit/RetrofitClient.java
@@ -12,7 +12,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://10.0.2.2:8000/api/v1/";
+    // private static final String BASE_URL = "http://10.0.2.2:8000/api/v1/";
+    private static final String BASE_URL = "https://recipe-manager-bvf8.onrender.com/api/v1/";
+
     private static Retrofit retrofit;
     private static  OkHttpClient okHttpClient;
     private static Context applicationContext; // Rename to applicationContext for clarity
